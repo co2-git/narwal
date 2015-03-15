783ca353f79a06e5ee2bde374014e272af92357c
francois <francoisrvespa@gmail.com>
2015-03-15 20:44:34 +0100

    hooks

--- index.js
+++ index.js
@@ -2,8 +2,30 @@
   
   'use strict';
 
-  exports.Client = require('./lib/Client');
-  exports.Model = require('./lib/Model');
-  exports.is = require('./lib/Is');
+  /** MODEL **/
+
+  exports.models = {};
+
+  function Model (name, structure, options) {
+    this.construct(name, structure, options);
+    exports.models[name] = this;
+  }
+
+  require('util').inherits(Model, require('./lib/Model'));
+
+  exports.Model = Model;
+
+  /** CLIENT **/
+
+  exports.clients = [];
+
+  function Client (url) {
+    this.construct(url);
+    exports.clients.push(this);
+  }
+
+  require('util').inherits(Client, require('./lib/Client'));
+
+  exports.Client = Client;
 
 } ();
